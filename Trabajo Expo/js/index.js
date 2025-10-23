62ca75cf400c71ff714772d32f25c8e468f7dee9
Xavo <losruizguevara@gmail.com>
2025-10-23 07:48:09 -0500

    respaldo 23/10/2025

--- Trabajo Expo/js/index.js
+++ Trabajo Expo/js/index.js
@@ -3,19 +3,20 @@ function ejecutarBucle() {
     let mensaje = "El bucle se ejecuta mientras el número sea menor que 5.<br>";
     mensaje += "<table><tr><th>Iteración</th><th>Condición (i < 5)</th></tr>";
 
-    // 1. EL BLOQUE DO...WHILE
+    
     do {
-        // Se ejecuta al menos una vez
-        contador++; // Aumenta 1 antes de la siguiente verificación
+       
+        contador++; 
         
-        // Agregar una fila a la tabla
+        
         mensaje += `<tr><td>${contador}</td><td>${contador < 5 ? 'VERDADERO' : 'FALSO'}</td></tr>`;
 
-    } while (contador < 5); // 2. Condición: Repetir si 'contador' es menor que 5
+    } while (contador < 5);
 
+    
     mensaje += "</table>";
     mensaje += `<p>✅ El bucle ha finalizado. Se ejecutó ${contador} veces.</p>`;
     
-    // Muestra el resultado en el elemento con id="resultado"
+   
     document.getElementById('resultado').innerHTML = mensaje;
 }
